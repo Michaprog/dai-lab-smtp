b322ce30c3db2979390b34ee0c47cb97f9d24a62
Michaprog <vejnstejn2@gmail.com>
2024-12-02 16:19:57 +0100

    addapt for txt

--- code/Client/src/main/java/ch/heig/dai/lab/smtp/Sender.java
+++ code/Client/src/main/java/ch/heig/dai/lab/smtp/Sender.java
@@ -2,33 +2,18 @@ package ch.heig.dai.lab.smtp;
 
 public class Sender extends Victim {
     private String username;
-    static private String password;
-    static private int port = 1234;
-    static private String host = "TODO";
 
-    public Sender(String email, String username, String password) {
+    public Sender(String email, String username) {
         super(email);
         this.username = username;
     }
 
     public static Sender castToSender(Victim victim){
-        return new Sender(victim.getEmail(), victim.getEmail().substring(0,6), password); // pas sur de la methodologie pour le username
+        return new Sender(victim.getEmail(), victim.getEmail().substring(0,6)); // pas sur de la methodologie pour le username
     }
 
     String getUsername() {
         return username;
-    }
-
-    String getPassword() {
-        return password;
-    }
-
-    int getPort() {
-        return port;
-    }
-
-    String getHost() {
-        return host;
     }
 
 }
